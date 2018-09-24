d8a4ecda332b96f09a65bb2d4e19c734c01f99d2
Omid Vatandoost <omidvatan94@gmail.com>
2018-09-24 18:49:14 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 # Add  code here!
-def prime?(integer)
-    return false if integer < 2
-    (2..integer - 1).each do |x|
-        if (integer % x) == 0
+def prime?(i)
+    return false if i < 2
+    (2..i - 1).each do |x|
+        if (i % x) == 0
             return false
         end
     end
